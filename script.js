b71cd59268914f18d0d020f9ac0b44899abb2d51
Eran Shiber <44651526+Shibex@users.noreply.github.com>
2018-12-27 00:20:17 +0200

    animation added

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const mobile = document.querySelector(".mob")
 const navBtn = document.querySelector(".burger")
 const mobLi = document.querySelector('.mob-ul');
+const box = document.querySelectorAll('.box');
+const iphone = document.querySelectorAll('.iphone-img');
 
 let toggle = false;
 navBtn.addEventListener("click", ()=> {
@@ -18,5 +20,21 @@ mobLi.addEventListener('click', (e) => {
     if(e.target.className === "mob-li"){       
         mobile.style.display = "none"
         toggle = false;
+    }
+})
+
+window.addEventListener("scroll", (e)=> {
+    console.log(scrollY)
+    if(scrollY > 300) {
+        box.forEach(item => {
+        item.style = "animation: fadeIn 4s"
+
+        })
+    }
+})
+
+window.addEventListener("scroll", (e) => {
+    if(scrollY > 1600) {
+        iphone[0].style = "animation: bounceInUp 2s";
     }
 })
